a08bd2d4f3ba47c627c4c13b410aa041c7f629cc
Michelle Yang <zomgitsmichelle@gmail.com>
2019-01-17 10:37:56 -0800

    Added all files and completed lab.

---
+++ static/js/introHCI.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+
+	$("a.thumbnail").click(projectClick);
+	
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+
+}
